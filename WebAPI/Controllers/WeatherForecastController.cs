2292f845e760585b4550cc1a25aca7d585567e9a
Raul Rodriguez <raurudesulml@gmail.com>
2023-12-12 20:15:20 -0600

    Se usuaron verbos HTTP

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -7,27 +7,69 @@ namespace WebAPI.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 
         private readonly ILogger<WeatherForecastController> _logger;
+
+        private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+
+            if (ListWeatherForecast == null || !ListWeatherForecast.Any())
+            {
+                ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToList();
+            }
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return ListWeatherForecast;
+        }
+
+        [HttpPost]
+        public IActionResult Post(WeatherForecast weatherForecast)
+        {
+            ListWeatherForecast.Add(weatherForecast);
+
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index >= ListWeatherForecast.Count)
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return NotFound(); // Devolver 404 si el índice está fuera de rango
+            }
+
+            ListWeatherForecast.RemoveAt(index);
+
+            return Ok();
+        }
+
+        [HttpPut("{index}")]
+        public IActionResult Put(int index, WeatherForecast updatedForecast)
+        {
+            if (index < 0 || index >= ListWeatherForecast.Count)
+            {
+                return NotFound(); // Devolver 404 si el índice está fuera de rango
+            }
+
+            // Actualizar el elemento en la posición especificada
+            ListWeatherForecast[index] = updatedForecast;
+
+            return Ok();
         }
     }
 }
